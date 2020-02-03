ca080bf43feaa334c4e90a1ca3569e992fde6447
Student <example@example.com>
2020-02-03 14:14:41 -0800

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace The_Epic_Legend_of_Codus.Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
